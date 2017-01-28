49d6b95c41d1a74476d887f70fd0362736c04dc4
Loïc Billou <Loc Billou>
2017-01-28 01:15:28 +0100

    commit

---
+++ index.test.js
@@ -0,0 +1,23 @@
+var expect = require('chai').expect;
+var describe = require('mocha').describe;
+
+//require('./src/components/allMovies/allMoviesContainer.js');
+//import AllMovies from './src/components/allMovies/allMoviesContainer.js';
+//var AllMovies = require('./src/components/allMovies/allMoviesContainer.js');
+
+/*
+import Footer from './src/components/allMovies/allMoviesContainer.js';
+
+describe('<Footer/>', () => {
+    it.only('should return a className prop as class value', () => {
+        const wrapper = shallow(<Foo className="bar" />);
+        console.log('DEBUG----->', wrapper.html());
+    });
+});*/
+
+
+describe('Unit Tests', function() {
+    it('True to be true ;) ', function () {
+        expect(true).to.be.true;
+    });
+});
